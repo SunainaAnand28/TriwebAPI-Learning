5263f7b5f7bc64975a7d50e9252fd7130d04581f
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-05-27 17:31:11 +0530

    Executed some literals.

---
+++ Programming/OOPs/literal.java
@@ -0,0 +1,18 @@
+public class literal {
+    public static void main(String[] args) {
+        byte age = 34;
+        System.out.println(age);
+
+        char var1 = 'A';
+        System.out.println(var1);
+
+        float f1 = 4.7f;
+        System.out.println(f1);
+
+        int num1 = 67;
+        System.out.println(num1);
+
+        double d1 = 9.00d;
+        System.out.println(d1);
+    }
+}
