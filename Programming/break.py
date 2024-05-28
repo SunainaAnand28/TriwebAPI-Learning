80a577cc54abc34565af3d1e0d8e58a936a04626
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-05-28 12:24:25 +0530

    Modified break.py

--- Programming/break.py
+++ Programming/break.py
@@ -1,7 +1,15 @@
 marks = [1,2,34,43,22,54]
+breakcalled = 0
 
 for i in marks:
     if i%11==0:
+        breakcalled = 1
         break
     print(i)
+
+if breakcalled == 1:
+    print("loop is breaked")
+else:
+    print("loop is completed")
+
 
