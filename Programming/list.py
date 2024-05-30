80050e8a7bfdad9537f47c5c6da189ce1bc361cb
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-05-30 23:22:08 +0530

    implemented list programs in python

---
+++ Programming/list.py
@@ -0,0 +1,15 @@
+li = [1,2,3,4,5,6,7]
+print(li)
+
+# append function to add any element at last index of given list...
+li.append(8)
+
+print("list after using append function")
+
+# after using append function
+print(li)
+
+#pull funciton
+li.remove(8)
+
+print("after removing 8\n ",li,)
