d655259510eebc1d06295b350fafc2545aad4a7d
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-05-31 23:17:18 +0530

    added .py for string slicing

---
+++ Programming/StringSlicing.py
@@ -0,0 +1,12 @@
+str = "HELLO THIS IS MY STRING"
+
+print(str)
+
+print("lenght of string is ",len(str))
+
+print(str[:4]) # using indexing to slice the string
+
+print(str[0:4]) # same output as above 
+
+print(str[0:-3]) # using negative indexing.
+
