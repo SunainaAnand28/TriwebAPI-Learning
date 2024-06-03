7e7ec7651559a3c53062b04537e965e4f9ff8d91
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-04 00:29:58 +0530

    Added program of concatination

---
+++ JavaScript/Day 01/6. Concatenation.js
@@ -0,0 +1,20 @@
+let a = "5";
+let b = 6;
+
+console.log(a+b); // 56
+console.log(typeof(a+b)); // string
+
+console.log("-----------------------------")
+
+const a1 = {
+    "name": "Sunaina",
+    class: "JavaScript",
+    chapter:1,
+    completed:false
+}
+a1["company"] = "Triweb-API";
+
+console.log(a1)
+console.log("-----------------------------")
+console.log(a1.class);
+console.log(a1['name']);
