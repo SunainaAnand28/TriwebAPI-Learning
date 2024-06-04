8777195118c2d811098c08e1c091bf68a4d8738c
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-05 00:33:18 +0530

    Added program of Text formatting

---
+++ JavaScript/Day 04/1. TextFormatting.js
@@ -0,0 +1,21 @@
+const description = "India is my country";
+console.log(description)
+
+const description1 = "'India' is my country";
+console.log(description1)
+
+const description2 = '"India" is my country';
+console.log(description2)
+
+const description3 = "'India' is my \"country\"";
+console.log(description3)
+
+const description4 = "India is my \'country\'";
+console.log(description4)
+
+const description5 = "'India' is my" + "'country'";
+console.log(description5)
+
+
+const description6 = "India is my\n country";
+console.log(description6)
