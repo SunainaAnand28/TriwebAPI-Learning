e3b8bd7ba70122689d4cd20a006bdc08ac65d893
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-05 00:25:32 +0530

    Added program of Error Handling

---
+++ JavaScript/Day 02/5. ErrorHandling.js
@@ -0,0 +1,17 @@
+try{
+
+
+    console.log(x); // ReferenceError 
+    console.log("Working");
+
+
+}
+catch{
+    // email to developer
+    // log error
+    console.log("server is under maintenance");
+
+}
+finally{
+    console.log("inside finally block")
+}
