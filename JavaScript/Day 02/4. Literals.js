c122fa4fc56978e41cb8bb979c55ae748c1a84a3
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-05 00:25:09 +0530

    Added Program of Literals

---
+++ JavaScript/Day 02/4. Literals.js
@@ -0,0 +1,39 @@
+console.log("decimal ", 117);
+console.log("Binary ", 0B0011);
+console.log("binary ", 0b0011);
+console.log("octal ", 0o77);
+console.log("hexa decimal ", 0x00111);
+
+const obj = {
+    "key": "value",
+    name: "sunaina",
+    "semester":  4,
+    "year": "second",
+    "passed": true,
+    "number": 503 
+}
+console.log(obj)
+console.log(obj.name)
+console.log("object literals with brackets")
+let obj1=
+{
+    "":"Hiii",
+    "!":"hello"
+
+}
+console.log(obj1[""]);
+console.log(obj["!"]);
+
+// String
+
+let s1 = 'string in single quote'
+let s2 = "string in double quote"
+
+let name1 = "Sunaina"
+let s3 = 'Hi '+name1+' how are you?';
+let s4 = `Hi ${name1} how are you?`;
+console.log(s1)
+console.log(s2)
+console.log(s3)
+console.log(s4)
+
