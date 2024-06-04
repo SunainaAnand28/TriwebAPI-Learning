2bd4dce2ded890108725f4b98cba53c16f30192c
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-05 00:24:12 +0530

    Implemented program of Datatype Conversion

---
+++ JavaScript/Day 02/2. DataType Converesion.js
@@ -0,0 +1,32 @@
+//conversion
+
+let n = 1;
+console.log(typeof(n));
+n = "hello";
+console.log(typeof(n));
+
+let num1 = "3";
+let num2 = 5;
+console.log(num1+num2);
+
+let num3 = "3";
+let num4 = 5;
+console.log(parseInt(num3)+num4);
+
+let num5 = "3.5";
+let num6 = 5;
+console.log(parseFloat(num5)+num6);
+
+let num7 = "8";
+let num8 = 5;
+console.log(+num7+num8);
+
+console.log(num7 - num8);
+console.log(num8 - num7);
+
+console.log(num8 * num7);
+console.log(num7 * num8);
+
+console.log(num8 / num7);
+console.log(num7 / num8);
+
