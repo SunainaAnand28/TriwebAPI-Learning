7073589f17e9094996bc67a21c2ab34623b11f63
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-05 00:24:43 +0530

    Added program of Number System

---
+++ JavaScript/Day 02/3. NumberSystem.js
@@ -0,0 +1,10 @@
+n = "101";
+console.log(parseInt(n));
+
+console.log(parseInt(n,2)); // Binary
+
+console.log(parseInt(n,8)); // Octal
+
+console.log(parseInt(n,10)); // Decimal
+
+console.log(parseInt(n,16)); // Hexa Decimal
