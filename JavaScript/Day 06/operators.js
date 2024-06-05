7a8b8749e770974191b42785a93321c8930fe325
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-06 00:16:30 +0530

    Added program of operators

---
+++ JavaScript/Day 06/operators.js
@@ -0,0 +1,33 @@
+let a = 45;
+let b = 47;
+
+console.log("Arithmetic Operators")
+
+console.log("a + b =  ", a+b);
+console.log("a - b =  ", a-b);
+console.log("a * b =  ", a*b);
+console.log("a / b =  ", a/b);
+console.log("a **3  =  ", a**3);
+console.log("a % b =  ", a%b);
+// increment operator
+
+console.log("++a = ",++a);
+console.log("a++ = ", a++)
+console.log("After a++ value of a = ",a);
+console.log("--a = ",--a);
+console.log("a-- = ",a--);
+
+
+console.log("-----------------------")
+console.log("Assignment Operators")
+let x = 1;
+x += 1;
+
+let  y = 1;
+ y += 1;
+let z = 4;
+ z -= 1;
+
+console.log(x,y,z)
+
+
