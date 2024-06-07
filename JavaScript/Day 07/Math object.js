f0640790fadd95a43056248cff2fa3e030298d07
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-07 12:37:14 +0530

    Added practice og day 7

---
+++ JavaScript/Day 07/Math object.js
@@ -0,0 +1,10 @@
+console.log(Math.PI);
+
+let max = Math.max(1,3,4,5,6,7);
+let min = Math.min(1,3,4,5,6,7);
+
+console.log("max",max);
+console.log("min",min);
+
+let ran = Math.random(); // between 0 - 1
+console.log(ran)
