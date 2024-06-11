857d87a7f348cfb585ea7b7f48503a764162b6ee
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-12 00:50:23 +0530

    Practiced Destructuring in ES6

---
+++ ES6/Destructuring.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3,4];
+let [a,b] = arr;
+
+console.log("a ", a);
+console.log("b ", b);
+[b,a]=[a,b];
+
+console.log("arr is  ", arr);
+console.log("a  ", a);
+console.log("b ", b);
+
