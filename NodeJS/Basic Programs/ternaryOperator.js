d83de75e67f49b9ed79138c98580213d417fab61
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-18 14:43:35 +0530

    Implemented some programs on ternary operators

---
+++ NodeJS/Basic Programs/ternaryOperator.js
@@ -0,0 +1,11 @@
+let a = 24;
+
+console.log("you can ",a >= a? "drive":"not drive");
+
+let b = 17;
+ console.log("you can", b >= 18? "vote": "not vote");
+ 
+
+const age = 26;
+const beverage = age >= 21 ? "ColdDrinks" : "Juice";
+console.log(beverage); // "ColdDrinks"
