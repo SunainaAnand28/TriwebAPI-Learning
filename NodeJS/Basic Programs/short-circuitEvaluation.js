c383ca07736b8a600f41df90d06f43420c9df497
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-20 00:51:47 +0530

    Practiced short- circuit evaluation

---
+++ NodeJS/Basic Programs/short-circuitEvaluation.js
@@ -0,0 +1,10 @@
+let a = 4;
+let b = 5;
+
+console.log(a||b); // 4
+console.log(a&&b); // 5
+ let a1 = "i am first and, here, i will return fisrt."
+ let b1 = "i am second and, here, i will return fisrt."
+
+ console.log(a1||b1); // a1
+ console.log(a1&&b1); // b1
