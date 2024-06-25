3043c814ab392266969103bdd2623a4eb073dc15
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-06-25 19:53:18 +0530

    Added React Application

---
+++ React/my-appnpx/src/App.js
@@ -0,0 +1,17 @@
+import Firstcomponent from "./components/Firstcomponent";
+import Backdrop from "./components/Backdrop";
+
+
+function App() {
+  return (
+   <div>
+    <h1>MY REACT APPLICATION</h1>
+      <Firstcomponent owner = "Sunaina " reason = "Learning React" />
+      <Backdrop></Backdrop>
+      
+      
+   </div>
+  );
+}
+
+export default App;
