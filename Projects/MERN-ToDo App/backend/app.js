3bba089f48a9151b2d9aa06dcb23b1173ca09fc1
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-07-04 19:42:50 +0530

    Added Modified Dependencies of todo project

--- Projects/MERN-ToDo App/backend/app.js
+++ Projects/MERN-ToDo App/backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const todoRouter =  require("./routers/todo");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/",todoRouter);
 
+const uri = "mongodb+srv://MyUser:abcd1234@mycluster.b0zn3ks.mongodb.net/"
+mongoose.set("strictQuery", false)
+mongoose.connect(uri);
 
-app.listen(3000,);
+app.listen(3002);
