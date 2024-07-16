7aa18581321482a6416ff14880d225153248ccd9
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-07-16 23:59:07 +0530

    Created the password validator and completed the user module.

--- Projects/QuizManager/Backend/src/routers/auth.ts
+++ Projects/QuizManager/Backend/src/routers/auth.ts
@@ -1,5 +1,5 @@
-import  express  from "express";
-import {registeruser, loginUser, isUserExist} from "../controllers/auth";
+import express from "express";
+import { registeruser, loginUser, isUserExist } from "../controllers/auth";
 import { body } from "express-validator";
 import ProjectError from "../helper/error";
 
@@ -9,31 +9,44 @@ const router = express.Router();
 
 
 //  post /auth/
-router.post("/",[
+router.post("/", [
     body('name')
-    .trim()
-    .not()
-    .isEmpty()
-    .isLength({min:4})
-    .withMessage("Please enter a valid name, minimum 4 character long"),
+        .trim()
+        .not()
+        .isEmpty()
+        .isLength({ min: 4 })
+        .withMessage("Please enter a valid name, minimum 4 character long"),
     body('email')
-    .trim()
-    .isEmail()
-    .custom(emailID => {
-     return isUserExist(emailID)
-     .then((status) =>{
-        if(status){
-          const err = new ProjectError("User already exist!");
-          err.statusCode = 422;
-          throw err;
-        }
-     })
-     .catch((err)=> {
-        return Promise.reject(err)
-     })
-    })
-    .normalizeEmail()
-],registeruser);
+        .trim()
+        .isEmail()
+        .custom((emailID:String) => {
+            return isUserExist(emailID)
+                .then((status:Boolean) => {
+                    if (status) {
+                        const err = new ProjectError("User already exist!");
+                        err.statusCode = 422;
+                        throw err;
+                    }
+                })
+                .catch((err) => {
+                    return Promise.reject(err)
+                })
+        })
+        .normalizeEmail(),
+        body('password')
+        .trim()
+        .isLength({min:6})
+        .withMessage('Enter atleast 6 character long password '),
+        body('confirm_password')
+        .trim()
+        .custom((value:String, {req})=>{
+            if(value != req.body.password){
+                return Promise.reject("password mismatch!")
+            }
+            return true;
+        })
+    
+], registeruser);
 
 // login /auth/login
 router.post('/login', loginUser);
